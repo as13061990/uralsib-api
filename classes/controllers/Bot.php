25e1a54adc09e63c6fbc29a467684e384df0ea2d
as13061990 <as13061990@yandex.ru>
2022-07-31 07:21:14 +0000

    added check subscribe

--- classes/controllers/Bot.php
+++ classes/controllers/Bot.php
@@ -6,12 +6,17 @@ class Bot extends \Basic\Basic {
 	 * точка входа чат-бота
 	 */
 	public static function main() {
-		$message = $_POST['message']['text'];
-
-		if ($message == '/start') {
-			$data = self::start();
+		if ($_POST['message']['entities'][0]['type'] != 'bot_command' && $_POST['callback_query']['data'] != 'checkSubscribe') {
+			exit();
 		}
+		$message = $_POST['message'] ? $_POST['message']['text'] : $_POST['callback_query']['data'];
 
+		if ($message == '/start' || $message == 'checkSubscribe') {
+			$data = self::start();
+		} else {
+			$data = self::badCommand();
+		}
+		
 		if ($data) {
 			self::sendTelegram('sendMessage', $data);
 		}
@@ -33,20 +38,96 @@ class Bot extends \Basic\Basic {
 		]);
 		$result = curl_exec($curl);
 		curl_close($curl);
+		return json_decode($result, true);
 	}
 
 	/**
 	 * стартовая команда
 	 */
 	private static function start() {
-		$id = $_POST['message']['from']['id'];
-
+		$id = $_POST['message'] ? $_POST['message']['from']['id'] : $_POST['callback_query']['from']['id'];
+		$chat = $_POST['message'] ? $_POST['message']['chat']['id'] : $_POST['callback_query']['message']['chat']['id'];
+		
 		if (is_numeric($id)) {
 			parent::checkUser($id);
-			// TODO: вставить ифчик на проверку подписки группы. Ретернем другой объект
+
+			if (!self::checkSubscribe($id)) {
+				$data = [
+					'text' => 'Чтобы начать игру, подпишись на канал <a href="https://t.me/bankuralsibnews">Банка Уралсиб</a> в Telegram',
+					'chat_id' => $chat,
+					'parse_mode' => 'HTML',
+					'reply_markup' => [
+						'inline_keyboard' => [
+							[
+								[
+									'text' => 'Открыть канал',
+									'url' => 'https://t.me/bankuralsibnews'
+								],
+								[
+									'text' => 'Проверить подписку',
+									"callback_data" => "checkSubscribe"
+								],
+							]
+						]
+					]
+				];
+				return $data;
+			}
 
 			$data = [
 				'text' => 'Привет! Покажи максимум ловкости в игре от Банка Уралсиб и получи до 1000 бонусных рублей на дебетовую карту "Прибыль"',
+				'chat_id' => $chat,
+				'reply_markup' => [
+					'inline_keyboard' => [
+						[
+							[
+								'text' => 'Играть',
+								'web_app' => ['url' => 'https://uralsib.irsapp.ru']
+							]
+						]
+					]
+				]
+			];
+			return $data;
+		}
+		return false;
+	}
+
+	/**
+	 * Любая другая команда
+	 */
+	private static function badCommand() {
+		$id = $_POST['message'] ? $_POST['message']['from']['id'] : $_POST['callback_query']['from']['id'];
+		$chat = $_POST['message'] ? $_POST['message']['chat']['id'] : $_POST['callback_query']['message']['chat']['id'];
+		
+		if (is_numeric($id)) {
+			parent::checkUser($id);
+
+			if (!self::checkSubscribe($id)) {
+				$data = [
+					'text' => 'Чтобы начать игру, подпишись на канал <a href="https://t.me/bankuralsibnews">Банка Уралсиб</a> в Telegram',
+					'chat_id' => $chat,
+					'parse_mode' => 'HTML',
+					'reply_markup' => [
+						'inline_keyboard' => [
+							[
+								[
+									'text' => 'Открыть канал',
+									'url' => 'https://t.me/bankuralsibnews'
+								],
+								[
+									'text' => 'Проверить подписку',
+									"callback_data" => "checkSubscribe"
+								],
+							]
+						]
+					]
+				];
+				return $data;
+			}
+
+			$data = [
+				'text' => 'Чтобы перейти к игре, нажми кнопку «Играть»',
 				'chat_id' => $_POST['message']['chat']['id'],
 				'reply_markup' => [
 					'inline_keyboard' => [
@@ -60,6 +141,27 @@ class Bot extends \Basic\Basic {
 				]
 			];
 			return $data;
+		}
+		return false;
+	}
+
+	/**
+	 * Проверка подписки на группу
+	 */
+	private static function checkSubscribe($id) {
+		// return true;
+
+		$data = [
+			'user_id' => $id,
+			'chat_id' => -1001191727950 //-1001776797334
+		];
+		$result = self::sendTelegram('getChatMember', $data);
+		
+		if ($result['result']['status'] == 'member' ||
+			$result['result']['status'] == 'creator' ||
+			$result['result']['status'] == 'administrator' ||
+			$result['result']['status'] == 'restricted') {
+			return true;
 		}
 		return false;
 	}
